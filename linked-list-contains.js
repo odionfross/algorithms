8fd1f669eae323c1fbd1003a0593209c6e7f60bb
Odion Fross <https://www.linkedin.com/in/odionfross/>
2020-09-24 20:52:48 -0700

    added contains method to sll

---
+++ linked-list-contains.js
@@ -0,0 +1,58 @@
+/* Contains
+
+Defining a node */
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+
+/* Contains
+Adding the contants method */
+
+class SLL {
+    constructor(value) {
+        this.head = new Node(value);
+    }
+    addFront(value) {
+        var restOfList = this.head;
+        this.head = new Node(value);
+        this.head.next = restOfList;
+        return this
+    }
+    removeFront() {
+        if (this.head != null) {
+            this.head = this.head.next;
+            return this
+        } else {
+            return null
+        }
+    }
+    front(){
+        if(this.head !== null){
+            return this.head.value
+        } else {
+            return "This is an empty list"
+        }
+    }
+
+    /* Add a method contains(value) to your SLL class, which is given a value as a parameter.  Return a boolean (true/false; true, if the list possesses a node that contains the provided value. */
+    contains(val){
+        var monkey = this.head
+        while(monkey){
+            if(monkey.value == val){
+                return true
+            }
+            monkey=monkey.next
+        }
+        return false
+    }
+}
+
+var one_sll = new SLL(1);
+//console.log(one_sll.addFront(3).addFront(5).addFront(7).addFront(11).addFront(9));
+//console.log(one_sll.addFront(3).addFront(5).addFront(7).removeFront(11).addFront(9));
+//console.log(one_sll.addFront(3).addFront(5).addFront(7).addFront(11).addFront(9).front());
+console.log(one_sll.addFront(3).addFront(5).addFront(7).addFront(11).addFront(9).contains(5));
