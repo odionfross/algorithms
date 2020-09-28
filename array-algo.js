9b7611267d0b9ecd68df1cfede6b933af8bdaed9
Odion Fross <https://www.linkedin.com/in/odionfross/>
2020-09-27 22:17:46 -0700

    array algorithms to do 2

--- array-algo.js
+++ array-algo.js
@@ -1,3 +1,5 @@
+/* To Do 1 */
+
 /* Min to Front
 Given an array of comparable values, move the lowest element to array’s front, shifting backward any elements previously ahead of it. Do not otherwise change the array’s order. Given [4,2,1,3,5], change it to [1,4,2,3,5] and return it. As always, do this without using built-in functions.*/
 
@@ -16,4 +18,137 @@ function minToFront(arr) {
     return arr;
 }
 
-console.log(minToFront([9,7,5,3,2,1]))
+//console.log(minToFront([9,7,5,3,2,1]))
+
+
+// Given an array and an additional value, insert this value at the beginning of the array. Do this without using any built-in array methods.
+function pushFront(arr, value) {
+	// move all values in array over
+	for(let i = arr.length; i > 0; i--)
+		arr[i] = arr[i-1]
+	
+	arr[0] = value;
+}
+
+// Given an array, remove and return the value at the beginning of the array. Do this without using any built-in array methods except pop().
+function popFront(arr) {
+	const val = arr[0];
+	for(let i = 0; i < arr.length; i++)
+		arr[i] = arr[i + 1];
+	arr.length = arr.length - 1;
+	return val;
+}
+
+
+// Given an array, index, and additional value, insert the value into array at given index. Do this without using built-in array methods. 
+// You can think of pushFront(arr,val) as equivalent to insertAt(arr,0,val).
+function insertAt(arr, idx, val) {
+	for(let i = arr.length; i > idx; i--)
+		arr[i] = arr[i-1]
+	
+	arr[idx] = val;
+}
+
+// Given an array and an index into array, remove and return the array value at that index. Do this without using built-in array methods except pop(). 
+// Think of popFront(arr) as equivalent to removeAt(arr,0).
+function removeAt(arr, idx) {
+    // shift array values right from idx
+	toRemove = arr[idx];
+	for(let i = idx; i < arr.length-1; i++) {
+        arr[i] = arr[i+1];
+    }
+    arr.length = arr.length-1;
+    return toRemove;
+}
+
+// Swap positions of successive pairs of values of given array. If length is odd, do not change the final element. For [1,2,3,4], return [2,1,4,3]. For example, change input ["Brendan",true,42] to [true,"Brendan",42]. 
+// As with all array challenges, do this without using any built-in array methods.
+function swapPairs(arr) {
+	for(let i = 0; i < arr.length - 1; i = i + 2) {
+		let temp = arr[i];
+		arr[i] = arr[i + 1];
+		arr[i + 1] = temp;
+	}
+}
+
+// Given a sorted array, remove duplicate values. Because array elements are already in order, all duplicate values will be grouped together. 
+function removeDupesUnnested(arr) {
+	let newArr = [];
+	for(let i = 0; i < arr.length; i++) {
+		if(arr[i] !== arr[i+1]) 
+			newArr.push(arr[i])
+	}
+	return newArr;
+}
+
+
+
+/* To Do 2
+----------
+
+Reverse
+Given a numerical array, reverse the order of values, in-place. The reversed array should have the same length, with existing elements moved to other indices so that order of elements is reversed. Working ‘in-place’ means that you cannot use a second array – move values within the array that you are given. As always, do not use built-in array functions such as splice(). */
+
+function reverse(arr) {
+    for(var i=0; i<arr.length/2; i++) {
+        var temp = arr[i];
+        arr[i] = arr[arr.length-1-i];
+        arr[arr.length-1-i] = temp;
+    }
+}
+//console.log(reverse([9,7,5,3,1]))
+
+/* Rotate
+Implement rotateArr(arr, shiftBy) that accepts array and offset. Shift arr’s values to the right by that amount. ‘Wrap-around’ any values that shift off array’s end to the other side, so that no data is lost. Operate in-place: given ([1,2,3],1), change the array to [3,1,2]. Don’t use built-in functions.
+
+Second: allow negative shiftBy (shift L, not R).
+
+Third: minimize memory usage. With no new array, handle arrays/shiftBys in the millions.
+
+Fourth: minimize the touches of each element. */
+
+function rotateArr(arr, shiftBy) {
+	let t = Math.abs(shiftBy);
+	while(t > 0) {
+
+		if(shiftBy > 0) {
+			let temp = arr[arr.length-1]
+			
+			for(let i = arr.length - 1; i > 0; i--)
+				arr[i] = arr[i-1];
+			arr[0] = temp;
+
+		} else {
+			let temp = arr[0];
+			for(let i = 0; i < arr.length - 1; i++) {
+				arr[i] = arr[i + 1];
+			}
+			arr[arr.length-1] = temp;
+		}
+		t--;
+	}
+}
+//console.log(rotateArr([1,2,3,4,5,6], 2))
+
+/* Filter Range
+Alan is good at breaking secret codes. One method is to eliminate values that lie within a specific known range. Given arr and values min and max, retain only the array values between min and max. Work in-place: return the array you are given, with values in original order. No built-in array functions. */
+
+function filterRange(arr, min, max) {
+	for(let i = 0; i < arr.length; i++) {
+		if(arr[i] > min && arr[i] < max) {
+			for(let j = i; j < arr.length - 1; j++) {
+                arr[j] = arr[j + 1];
+            }
+            arr.length = arr.length - 1;
+			i--;
+		}
+	}
+}
+//console.log(filterRange([5,4,3,2,1],1,5))
+
+/* Concat
+Replicate JavaScript’s concat(). Create a standalone function that accepts two arrays. Return a new array containing the first array’s elements, followed by the second array’s elements. Do not alter the original arrays. Ex.: arrConcat( ['a','b'], [1,2] ) should return new array ['a','b',1,2]. */
+
+function arrConcat(arr1, arr2) {
+	
+}
